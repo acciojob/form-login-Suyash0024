0130fc331121c6959b38c031fb6263497a01d989
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-08-05 20:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 function getFormvalue() {
     //Write your code here
+	const form = document.getElementByID('form1');
+
+	const firtName = form.elements['fname'].value.trim();
+	const lastName = form.elements['lname'].value.trim();
+
+	alert(`${firstName} ${lastName}`);
+
+	return false;
+	
 
 }
