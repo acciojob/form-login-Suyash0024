eb0a43a1c6e1df4bb70b4807e12ed7ef44d4d693
Suyash Garud <113934597+Suyash0024@users.noreply.github.com>
2025-08-05 20:11:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getFormvalue() {
     //Write your code here
-	const form = document.getElementByID('form1');
+	const form = document.getElementById('form1');
 
 	const firtName = form.elements['fname'].value.trim();
 	const lastName = form.elements['lname'].value.trim();
